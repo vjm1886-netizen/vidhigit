cf28d26a02a6f50af45c415e886a0e6a87d01ba5
vjm1886-netizen <vjm1886@gmail.com>
2026-02-15 23:28:20 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ class Addition{
     public static void main (String[] args){
         int a=10;
         int b=20;
-        int sum=a*b;
+        int sum=a+b;
         System.out.println("Multiply: "*sum);
         
     }
