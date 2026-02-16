99be39e157b5d151a73891d35d03e01fb3a3b58c
vjm1886-netizen <vjm1886@gmail.com>
2026-02-16 15:52:18 +0530

    Delete sum.java

--- sum.java
+++
@@ -1,9 +0,0 @@
-class Addition{
-    public static void main (String[] args){
-        int a=10;
-        int b=20;
-        int sum=a+b;
-        System.out.println("Subtract: "-sum);
-        
-    }
-}
